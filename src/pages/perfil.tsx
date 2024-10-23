8639e703eeeafebb28a93bbaddbd6ea4072e32d2
MateoV <mvroonland@fi.uba.ar>
2024-10-23 00:45:11 -0300

    style: run prettier formatter

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -35,104 +35,104 @@ export default function UserProfileEdit() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log({ name, email, gender, country, birthdate});
+    console.log({ name, email, gender, country, birthdate });
   };
 
   return (
     <div>
       <div className="min-h-screen bg-slate-100">
-        <div className="flex items-center justify-between bg-slate-800 px-4 py-4 text-slate-200 shadow shadow-slate-200 h-[70px]">
+        <div className="flex h-[70px] items-center justify-between bg-slate-800 px-4 py-4 text-slate-200 shadow shadow-slate-200">
           <h1 className="text-xl font-semibold">Polimardo Reads</h1>
           <Undo2 onClick={() => router.push("/")} />
         </div>
-        <div className="items-center position-relative mx-auto mt-20">
-        <Card className="mx-auto max-w-2xl pl-10 pr-10 pt-10 pb-10">
-          <CardHeader>
-            <div className="flex items-center space-x-4">
-              <Avatar className="h-20 w-20">
-                <AvatarImage src={session?.user?.image || ""} alt={name} />
-                <AvatarFallback>
-                  <User className="h-10 w-10" />
-                </AvatarFallback>
-              </Avatar>
-              <div>
-                <CardTitle className="text-2xl">Edita tu perfil</CardTitle>
-                <CardDescription>
-                  Actualiza tu información personal
-                </CardDescription>
+        <div className="position-relative mx-auto mt-20 items-center">
+          <Card className="mx-auto max-w-2xl pb-10 pl-10 pr-10 pt-10">
+            <CardHeader>
+              <div className="flex items-center space-x-4">
+                <Avatar className="h-20 w-20">
+                  <AvatarImage src={session?.user?.image || ""} alt={name} />
+                  <AvatarFallback>
+                    <User className="h-10 w-10" />
+                  </AvatarFallback>
+                </Avatar>
+                <div>
+                  <CardTitle className="text-2xl">Edita tu perfil</CardTitle>
+                  <CardDescription>
+                    Actualiza tu información personal
+                  </CardDescription>
+                </div>
               </div>
-            </div>
-          </CardHeader>
-          <form onSubmit={handleSubmit}>
-            <CardContent className="space-y-6">
-              <div className="space-y-2">
-                <Label htmlFor="name">Nombre Usuario</Label>
-                <Input
-                  id="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                />
-              </div>
-
-              <div className="space-y-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-
-              <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            </CardHeader>
+            <form onSubmit={handleSubmit}>
+              <CardContent className="space-y-6">
                 <div className="space-y-2">
-                  <Label htmlFor="gender">Genero</Label>
-                  <Select value={gender} onValueChange={setGender}>
-                    <SelectTrigger id="gender">
-                      <SelectValue placeholder="Seleccionar genero" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      <SelectItem value="male">Masculino</SelectItem>
-                      <SelectItem value="female">Femenino</SelectItem>
-                    </SelectContent>
-                  </Select>
+                  <Label htmlFor="name">Nombre Usuario</Label>
+                  <Input
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
                 </div>
 
                 <div className="space-y-2">
-                  <Label htmlFor="country">Pais</Label>
-                  <Select value={country} onValueChange={setCountry}>
-                    <SelectTrigger id="country">
-                      <SelectValue placeholder="Seleccionar pais" />
-                    </SelectTrigger>
-                    <SelectContent>
-                      {country_list.map((country: string) => (
-                        <SelectItem key={country} value={country}>
-                          {country}
-                        </SelectItem>
-                      ))}
-                    </SelectContent>
-                  </Select>
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
-              </div>
 
-              <div className="space-y-2">
-                <Label htmlFor="birthdate">Fecha de nacimiento</Label>
-                <Input
-                  id="birthdate"
-                  value={birthdate?.toISOString().split("T")[0]}
-                  onChange={(e) => setBirthdate(new Date(e.target.value))}
-                  type="date"
-                  className={cn("pl-10", "text-slate-600")}
-                />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button type="submit" className="w-full">
-                Guardar Cambios
-              </Button>
-            </CardFooter>
-          </form>
-        </Card>
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                  <div className="space-y-2">
+                    <Label htmlFor="gender">Genero</Label>
+                    <Select value={gender} onValueChange={setGender}>
+                      <SelectTrigger id="gender">
+                        <SelectValue placeholder="Seleccionar genero" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="male">Masculino</SelectItem>
+                        <SelectItem value="female">Femenino</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+
+                  <div className="space-y-2">
+                    <Label htmlFor="country">Pais</Label>
+                    <Select value={country} onValueChange={setCountry}>
+                      <SelectTrigger id="country">
+                        <SelectValue placeholder="Seleccionar pais" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        {country_list.map((country: string) => (
+                          <SelectItem key={country} value={country}>
+                            {country}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
+
+                <div className="space-y-2">
+                  <Label htmlFor="birthdate">Fecha de nacimiento</Label>
+                  <Input
+                    id="birthdate"
+                    value={birthdate?.toISOString().split("T")[0]}
+                    onChange={(e) => setBirthdate(new Date(e.target.value))}
+                    type="date"
+                    className={cn("pl-10", "text-slate-600")}
+                  />
+                </div>
+              </CardContent>
+              <CardFooter>
+                <Button type="submit" className="w-full">
+                  Guardar Cambios
+                </Button>
+              </CardFooter>
+            </form>
+          </Card>
         </div>
       </div>
     </div>
