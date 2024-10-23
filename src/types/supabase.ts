1069cf426b6fcad4cf69e94299624825ec2d4522
Juani <jperezdichiazza@gmail.com>
2024-10-22 22:34:08 -0300

    feat: creation of user

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -4,243 +4,266 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   next_auth: {
     Tables: {
       accounts: {
         Row: {
-          access_token: string | null;
-          expires_at: number | null;
-          id: string;
-          id_token: string | null;
-          oauth_token: string | null;
-          oauth_token_secret: string | null;
-          provider: string;
-          providerAccountId: string;
-          refresh_token: string | null;
-          scope: string | null;
-          session_state: string | null;
-          token_type: string | null;
-          type: string;
-          userId: string | null;
-        };
+          access_token: string | null
+          expires_at: number | null
+          id: string
+          id_token: string | null
+          oauth_token: string | null
+          oauth_token_secret: string | null
+          provider: string
+          providerAccountId: string
+          refresh_token: string | null
+          scope: string | null
+          session_state: string | null
+          token_type: string | null
+          type: string
+          userId: string | null
+        }
         Insert: {
-          access_token?: string | null;
-          expires_at?: number | null;
-          id?: string;
-          id_token?: string | null;
-          oauth_token?: string | null;
-          oauth_token_secret?: string | null;
-          provider: string;
-          providerAccountId: string;
-          refresh_token?: string | null;
-          scope?: string | null;
-          session_state?: string | null;
-          token_type?: string | null;
-          type: string;
-          userId?: string | null;
-        };
+          access_token?: string | null
+          expires_at?: number | null
+          id?: string
+          id_token?: string | null
+          oauth_token?: string | null
+          oauth_token_secret?: string | null
+          provider: string
+          providerAccountId: string
+          refresh_token?: string | null
+          scope?: string | null
+          session_state?: string | null
+          token_type?: string | null
+          type: string
+          userId?: string | null
+        }
         Update: {
-          access_token?: string | null;
-          expires_at?: number | null;
-          id?: string;
-          id_token?: string | null;
-          oauth_token?: string | null;
-          oauth_token_secret?: string | null;
-          provider?: string;
-          providerAccountId?: string;
-          refresh_token?: string | null;
-          scope?: string | null;
-          session_state?: string | null;
-          token_type?: string | null;
-          type?: string;
-          userId?: string | null;
-        };
+          access_token?: string | null
+          expires_at?: number | null
+          id?: string
+          id_token?: string | null
+          oauth_token?: string | null
+          oauth_token_secret?: string | null
+          provider?: string
+          providerAccountId?: string
+          refresh_token?: string | null
+          scope?: string | null
+          session_state?: string | null
+          token_type?: string | null
+          type?: string
+          userId?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: "accounts_userId_fkey";
-            columns: ["userId"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
+            foreignKeyName: "accounts_userId_fkey"
+            columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
           },
-        ];
-      };
+        ]
+      }
       sessions: {
         Row: {
-          expires: string;
-          id: string;
-          sessionToken: string;
-          userId: string | null;
-        };
+          expires: string
+          id: string
+          sessionToken: string
+          userId: string | null
+        }
         Insert: {
-          expires: string;
-          id?: string;
-          sessionToken: string;
-          userId?: string | null;
-        };
+          expires: string
+          id?: string
+          sessionToken: string
+          userId?: string | null
+        }
         Update: {
-          expires?: string;
-          id?: string;
-          sessionToken?: string;
-          userId?: string | null;
-        };
+          expires?: string
+          id?: string
+          sessionToken?: string
+          userId?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: "sessions_userId_fkey";
-            columns: ["userId"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
+            foreignKeyName: "sessions_userId_fkey"
+            columns: ["userId"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
           },
-        ];
-      };
+        ]
+      }
       users: {
         Row: {
-          birth_date: string | null;
-          country: string | null;
-          email: string | null;
-          emailVerified: string | null;
-          gender: string | null;
-          id: string;
-          image: string | null;
-          name: string | null;
-          onboarding_completed: boolean;
-        };
+          birth_date: string | null
+          country: string | null
+          email: string | null
+          emailVerified: string | null
+          gender: string | null
+          id: string
+          image: string | null
+          name: string | null
+          onboarding_completed: boolean
+        }
         Insert: {
-          birth_date?: string | null;
-          country?: string | null;
-          email?: string | null;
-          emailVerified?: string | null;
-          gender?: string | null;
-          id?: string;
-          image?: string | null;
-          name?: string | null;
-          onboarding_completed?: boolean;
-        };
+          birth_date?: string | null
+          country?: string | null
+          email?: string | null
+          emailVerified?: string | null
+          gender?: string | null
+          id?: string
+          image?: string | null
+          name?: string | null
+          onboarding_completed?: boolean
+        }
         Update: {
-          birth_date?: string | null;
-          country?: string | null;
-          email?: string | null;
-          emailVerified?: string | null;
-          gender?: string | null;
-          id?: string;
-          image?: string | null;
-          name?: string | null;
-          onboarding_completed?: boolean;
-        };
-        Relationships: [];
-      };
+          birth_date?: string | null
+          country?: string | null
+          email?: string | null
+          emailVerified?: string | null
+          gender?: string | null
+          id?: string
+          image?: string | null
+          name?: string | null
+          onboarding_completed?: boolean
+        }
+        Relationships: []
+      }
       verification_tokens: {
         Row: {
-          expires: string;
-          identifier: string | null;
-          token: string;
-        };
+          expires: string
+          identifier: string | null
+          token: string
+        }
         Insert: {
-          expires: string;
-          identifier?: string | null;
-          token: string;
-        };
+          expires: string
+          identifier?: string | null
+          token: string
+        }
         Update: {
-          expires?: string;
-          identifier?: string | null;
-          token?: string;
-        };
-        Relationships: [];
-      };
-    };
+          expires?: string
+          identifier?: string | null
+          token?: string
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
       uid: {
-        Args: Record<PropertyKey, never>;
-        Returns: string;
-      };
-    };
+        Args: Record<PropertyKey, never>
+        Returns: string
+      }
+    }
     Enums: {
-      gender: "male" | "female";
-    };
+      gender: "male" | "female"
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
   public: {
     Tables: {
       books: {
         Row: {
-          book_author: string;
-          book_title: string;
-          created_at: string;
-          id: string;
-          image_ur_1: string | null;
-          image_ur_2: string | null;
-          image_ur_3: string | null;
-          isbn: number;
-          publish_year: number;
-          publisher: string;
-        };
+          book_author: string
+          book_title: string
+          created_at: string
+          id: string
+          image_url_1: string | null
+          image_url_2: string | null
+          image_url_3: string | null
+          isbn: string
+          publish_year: number
+          publisher: string
+        }
         Insert: {
-          book_author: string;
-          book_title: string;
-          created_at?: string;
-          id?: string;
-          image_ur_1?: string | null;
-          image_ur_2?: string | null;
-          image_ur_3?: string | null;
-          isbn: number;
-          publish_year: number;
-          publisher: string;
-        };
+          book_author: string
+          book_title: string
+          created_at?: string
+          id?: string
+          image_url_1?: string | null
+          image_url_2?: string | null
+          image_url_3?: string | null
+          isbn: string
+          publish_year: number
+          publisher: string
+        }
         Update: {
-          book_author?: string;
-          book_title?: string;
-          created_at?: string;
-          id?: string;
-          image_ur_1?: string | null;
-          image_ur_2?: string | null;
-          image_ur_3?: string | null;
-          isbn?: number;
-          publish_year?: number;
-          publisher?: string;
-        };
-        Relationships: [];
-      };
+          book_author?: string
+          book_title?: string
+          created_at?: string
+          id?: string
+          image_url_1?: string | null
+          image_url_2?: string | null
+          image_url_3?: string | null
+          isbn?: string
+          publish_year?: number
+          publisher?: string
+        }
+        Relationships: []
+      }
       books_genres: {
         Row: {
-          genre_name: string;
-          id: string;
-        };
+          id: string
+          name: string
+        }
         Insert: {
-          genre_name: string;
-          id?: string;
-        };
+          id?: string
+          name: string
+        }
         Update: {
-          genre_name?: string;
-          id?: string;
-        };
-        Relationships: [];
-      };
-    };
+          id?: string
+          name?: string
+        }
+        Relationships: []
+      }
+      user_favorite_genres: {
+        Row: {
+          genre_id: string
+          user_id: string
+        }
+        Insert: {
+          genre_id: string
+          user_id: string
+        }
+        Update: {
+          genre_id?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "user_favorite_genres_genre_id_fkey"
+            columns: ["genre_id"]
+            isOneToOne: false
+            referencedRelation: "books_genres"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type PublicSchema = Database[Extract<keyof Database, "public">];
+type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -253,7 +276,7 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
@@ -261,11 +284,11 @@ export type Tables<
         PublicSchema["Views"])
     ? (PublicSchema["Tables"] &
         PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R;
+        Row: infer R
       }
       ? R
       : never
-    : never;
+    : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -276,17 +299,17 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I;
+        Insert: infer I
       }
       ? I
       : never
-    : never;
+    : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -297,17 +320,17 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U;
+        Update: infer U
       }
       ? U
       : never
-    : never;
+    : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -320,14 +343,14 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never;
+    : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof PublicSchema["CompositeTypes"]
     | { schema: keyof Database },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof Database;
+    schema: keyof Database
   }
     ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
     : never = never,
@@ -335,4 +358,4 @@ export type CompositeTypes<
   ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
     ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-    : never;
+    : never
