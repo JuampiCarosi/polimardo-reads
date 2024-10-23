902019fd550bda8607e1c03a787a86921622bd28
juampicarosi <juampicarosi@gmail.com>
2024-10-23 17:02:23 -0300

    actualy update profile

--- src/pages/perfil.tsx
+++ src/pages/perfil.tsx
@@ -25,6 +25,8 @@ import { country_list } from "@/pages/welcome";
 import { useRouter } from "next/router";
 import { type Session } from "next-auth";
 import Link from "next/link";
+import axios from "axios";
+import { toast } from "sonner";
 
 function Form({ user }: { user: Session["user"] }) {
   const [name, setName] = useState(user.name!);
@@ -33,9 +35,21 @@ function Form({ user }: { user: Session["user"] }) {
   const [country, setCountry] = useState(user.country);
   const [birthdate, setBirthdate] = useState(user.birth_date);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log({ name, email, gender, country, birthdate });
+  const handleSubmit = async () => {
+    const user = {
+      name,
+      email,
+      gender,
+      country,
+      birth_date: birthdate,
+    };
+    try {
+      await axios.put("/api/users", { user });
+      toast.success("Usuario actualizado correctamente");
+    } catch (err) {
+      console.error(err);
+      toast.error("Error al actualizar el usuario");
+    }
   };
 
   return (
@@ -54,75 +68,73 @@ function Form({ user }: { user: Session["user"] }) {
           </div>
         </div>
       </CardHeader>
-      <form onSubmit={handleSubmit}>
-        <CardContent className="space-y-6">
+      <CardContent className="space-y-6">
+        <div className="space-y-2">
+          <Label htmlFor="name">Nombre Usuario</Label>
+          <Input
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
           <div className="space-y-2">
-            <Label htmlFor="name">Nombre Usuario</Label>
-            <Input
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-            <div className="space-y-2">
-              <Label htmlFor="gender">Genero</Label>
-              <Select value={gender ?? undefined} onValueChange={setGender}>
-                <SelectTrigger id="gender">
-                  <SelectValue placeholder="Seleccionar genero" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="male">Masculino</SelectItem>
-                  <SelectItem value="female">Femenino</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-
-            <div className="space-y-2">
-              <Label htmlFor="country">Pais</Label>
-              <Select value={country ?? undefined} onValueChange={setCountry}>
-                <SelectTrigger id="country">
-                  <SelectValue placeholder="Seleccionar pais" />
-                </SelectTrigger>
-                <SelectContent>
-                  {country_list.map((country: string) => (
-                    <SelectItem key={country} value={country}>
-                      {country}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
-            </div>
+            <Label htmlFor="gender">Genero</Label>
+            <Select value={gender ?? undefined} onValueChange={setGender}>
+              <SelectTrigger id="gender">
+                <SelectValue placeholder="Seleccionar genero" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="male">Masculino</SelectItem>
+                <SelectItem value="female">Femenino</SelectItem>
+              </SelectContent>
+            </Select>
           </div>
 
           <div className="space-y-2">
-            <Label htmlFor="birthdate">Fecha de nacimiento</Label>
-            <Input
-              id="birthdate"
-              value={birthdate ?? undefined}
-              onChange={(e) => setBirthdate(e.target.value)}
-              type="date"
-              className={cn("pl-10", "text-slate-600")}
-            />
+            <Label htmlFor="country">Pais</Label>
+            <Select value={country ?? undefined} onValueChange={setCountry}>
+              <SelectTrigger id="country">
+                <SelectValue placeholder="Seleccionar pais" />
+              </SelectTrigger>
+              <SelectContent>
+                {country_list.map((country: string) => (
+                  <SelectItem key={country} value={country}>
+                    {country}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
           </div>
-        </CardContent>
-        <CardFooter>
-          <Button type="submit" className="w-full">
-            Guardar Cambios
-          </Button>
-        </CardFooter>
-      </form>
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="birthdate">Fecha de nacimiento</Label>
+          <Input
+            id="birthdate"
+            value={birthdate ?? undefined}
+            onChange={(e) => setBirthdate(e.target.value)}
+            type="date"
+            className={cn("pl-10", "text-slate-600")}
+          />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleSubmit} className="w-full">
+          Guardar Cambios
+        </Button>
+      </CardFooter>
     </Card>
   );
 }
