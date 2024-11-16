767c26023c5893ddf5c544e84f13be7c7c6fbdb8
MateoV <mvroonland@fi.uba.ar>
2024-11-16 13:49:24 -0300

    feat: add endpoint to edit challenges

--- src/pages/api/challenges/[id].ts
+++ src/pages/api/challenges/[id].ts
@@ -1,5 +1,6 @@
 import { supabase } from "@/server/supabase";
 import { type NextApiHandler } from "next";
+import { z } from "zod";
 
 export type Challenge = {
   id: string;
@@ -12,6 +13,17 @@ export type Challenge = {
   book_ids: string[];
 };
 
+const putSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  description: z.string(),
+  created_by: z.string(),
+  start_date: z.string(),
+  end_date: z.string(),
+  participants: z.number(),
+  book_ids: z.array(z.string()),
+});
+
 const handler: NextApiHandler = async (req, res) => {
   const challenge_id = req.query.id;
 
@@ -23,6 +35,38 @@ const handler: NextApiHandler = async (req, res) => {
     const { data, error } = await supabase.rpc("get_challenge", {
       challenge_id,
     });
+
+    if (error) {
+      console.error(error);
+      return res.status(500).json({ error: error.message });
+    }
+
+    if (!data?.[0]) {
+      return res.status(404).json({ error: "Challenge not found" });
+    }
+
+    return res.status(200).json(data[0] satisfies Challenge);
+  }
+
+  if (req.method === "PUT") {
+    const result = putSchema.safeParse(req.body);
+    if (!result.success) {
+      console.log(result.error.message);
+      return res.status(400).json({ error: result.error });
+    }
+
+    const challengeData = {
+      id: result.data.id,
+      name: result.data.name,
+      description: result.data.description,
+      created_by: result.data.created_by,
+      start_date: result.data.start_date,
+      end_date: result.data.end_date,
+      participants: result.data.participants,
+      book_ids: result.data.book_ids,
+    };
+
+    const { data, error } = await supabase.from("challenges").upsert(challengeData);
 
     if (error) {
       console.error(error);
